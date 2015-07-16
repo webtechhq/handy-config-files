6442237cbcd32c1a6350d082929cc45a13025344
James S. Moore <moorej@ipwork.ipsoft.com>
2015-07-16 10:01:20 -0500

    Cleanup and addition of Zenburn theme.  Also disabled startup message

--- emacs/.emacs.el
+++ emacs/.emacs.el
@@ -1,16 +1,15 @@
-;disable backup
+;;disable backup
 (setq backup-inhibited t)
-;disable auto save
+;;disable auto save
 (setq auto-save-default nil)
+;;disable startup message screen
+(setq inhibit-startup-message t)
 
 ;; to setup tabs
 (setq c-basic-indent 2)
 (setq tab-width 4)
 (setq indent-tabs-mode nil)
 
-(add-to-list 'load-path "/home/tempus/.emacs.d/color-theme-6.6.0/color-theme.el")
-(require 'color-theme)
-(eval-after-load "color-theme"
-  '(progn
-     (color-theme-initialize)
-     (color-theme-hober)))
+;; add my custom theme path and load zenburn
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'zenburn t)
